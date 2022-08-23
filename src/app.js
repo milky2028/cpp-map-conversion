f661dc5055b1d7d0bdd6d824fd2cf20630c8fd14
Tyler Gross <tylergross28@gmail.com>
2022-08-23 14:20:11 -0400

    basic functionality working

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const Module = require("../build/lib");
+
+async function main() {
+  const instance = await Module();
+  const app = new instance.Application();
+
+  console.log(app.clipboard());
+}
+
+main();
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,15 @@
+#include <emscripten/bind.h>
 #include <iostream>
 #include <map>
 #include <memory>
 
-struct clipboard_entry {
-    std::shared_ptr<void> _buffer;
-    std::size_t _size{0};
-
-    friend bool operator==(const clipboard_entry& x, const clipboard_entry& y) {
-        return x._buffer == y._buffer && x._size == y._size;
-    }
-
-    friend bool operator!=(const clipboard_entry& x, const clipboard_entry& y) {
-        return !(x == y);
-    }
+class Application {
+ public:
+  bool clipboard() { return true; }
 };
 
-using clipboard_t = std::map<std::string, int>;
-
-int main() {
-  std::map<std::string, int> some_map {{"one", 1}, {"two", 2}, { "three", 3}};
-  std::cout << some_map["two"] << std::endl;
+EMSCRIPTEN_BINDINGS(Application) {
+  emscripten::class_<Application>("Application")
+      .constructor()
+      .function("clipboard", &Application::clipboard);
 }
